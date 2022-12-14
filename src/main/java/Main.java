cc88a22e5a639301f54e8e69baa65c34373964cd
Djordje Dimitrov <110050027+djolodjolo23@users.noreply.github.com>
2022-12-14 13:43:57 +0100

    Added some comments inside the main.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,8 @@ import java.util.concurrent.Semaphore;
 public class Main {
 
   public static void main(String[] args) throws InterruptedException {
+    // starting the intial permits as 0 so the worker A can get to job first
+    // worker A releases one permit at the start of the run().
     Semaphore semaphore = new Semaphore(0);
 
     WorkerA wa = new WorkerA(semaphore);
